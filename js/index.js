677819afd1db8f7d01e4e787a37c54d9046404f9
Eldad David <davengash100@gmail.com>
2021-06-14 08:58:56 +0300

    add function to the form element in html

--- js/index.js
+++ js/index.js
@@ -1,23 +1,26 @@
-function calculatingAkanname (day, month, year, century) {
-    let dd = day;
-    let mm =  month;
-    let yy = year;
-    let cc = century;
-    return ( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) % 7;
+function calculatingAkanname () {
+
+    let dd = parseInt(document.getElementById('birthday').value);
+    let mm = parseInt(document.getElementById('birthmonth').value);
+    let yy = parseInt(document.getElementById('birthyear').value);
+    let cc = parseInt(document.getElementById('birthcentury').value);
+
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    let maleName = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
+    let femaleName = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+
+    if (dd.value == '' || mm.value == ''|| yy.value == ''|| cc.value == '') {
+        alert ('No blank values allowed.');
+    }
+
+    let gender = document.querySelector('input[]')
+    
 }
 
 function getDayandName () {
-    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-    let maleName = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
-    let femaleName = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'] 
-    let gender = "";
+    
+    
     let index = calculatingAkanname ();
     let day = days [index];
     let name = '';
-    if (gender === 'Male') {
-        name = maleName [index];
-    }
-    else {
-        name = femaleName [index];
-    }
-}
+    
